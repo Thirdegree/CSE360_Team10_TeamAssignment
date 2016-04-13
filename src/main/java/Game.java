2eb9bea3ec0ab66b569f6c989a26f3d84518b5e5
JulianC-M <jcruzemi@gmail.com>
2016-04-13 15:00:06 -0700

    Adding  Game and Stats

---
+++ src/main/java/Game.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @author JulianC-M
+ * My idea is to create two game with different tracks, and we vote specificly on
+ * the game rather than just on a single track. propbably make it easier
+ * have each player with a vote command, with Game as param, and each player with
+ * an instance of game can count as one vote towards the game
+ */
+public class Game{
+	Track currentTrack;
+	int numPlayers;
+	int numVotes;
+	
+	public Game(int numOfPlayers, Track newTrack)
+	{
+		currentTrack =  newTrack;
+		numPlayers = numOfPlayers;
+		numVotes = 0;
+	}
+	
+	int getNumVotes()
+	{
+		return numVotes;
+	}
+	
+	Track getTrack()
+	{
+		return currentTrack;
+	}
+	
+	void Vote()
+	{
+		numVotes++;
+	}
+	///Think about making Race static, so all players can interact and so each can use
+	///race even if it is created by Game
+	///player.game.race._______________
+	void startRace()
+	{
+		Race newRace = new Race(currentTrack,numPlayers);
+	}
+}
---
+++ src/main/java/Stats.java
@@ -0,0 +1,12 @@
+
+public class Stats
+{
+	int numSkip;
+	int currentPosition;
+	
+	public Stats()
+	{
+		
+	}
+	
+}
