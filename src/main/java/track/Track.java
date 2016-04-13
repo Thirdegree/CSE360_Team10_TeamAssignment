d2bfeea913717e0a29f389c3b9c6869bdc51e310
Joel Christiansen <greenbadge.jc@gmail.com>
2016-04-13 14:35:39 -0700

    added variables to track

--- src/main/java/track/Track.java
+++ src/main/java/track/Track.java
@@ -7,11 +7,23 @@ package track;
 public class Track {
 	private double disasters = 0.05;
 	private double blessings = 0.05;
+	private String startLocation;
+	private String endLocation;
+	private int length;
 	
 	public double getDisasters() {
 		return disasters;
 	}
 	public double getBlessings() {
 		return blessings;
+	}
+	public String getStartLocation() {
+		return startLocation;
+	}
+	public String getEndLocation() {
+		return endLocation;
+	}
+	public int getLength() {
+		return length;
 	}
 }
