bb9b26d743e24382d979b9c6386a70a5aa858370
hayzoos81 <jerm228@asu.edu>
2016-04-14 11:39:30 -0700

    Added updateProfile()

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import car.Car;
+import java.util.Scanner;
 
 /**
  * 
@@ -74,13 +76,17 @@ public class Player {
 		return this.currentCar;
 	}
 	
+	public void setUserName(String newName) {
+		this.userName = newName;
+	}
+	
 	/**
 	 * Updates the player's total score.
 	 * 
 	 * @param score
 	 */
 	public void updateScore(int score) {
-		totalScore += score;
+		this.totalScore += score;
 	}
 	
 	/**
@@ -89,7 +95,7 @@ public class Player {
 	 * @param money
 	 */
 	public void updateMoney(double money) {
-		totalMoney += money;
+		this.totalMoney += money;
 	}
 	
 	/**
@@ -98,6 +104,74 @@ public class Player {
 	 * @param newCar
 	 */
 	public void updateCar(Car newCar) {
-		currentCar = newCar;
+		this.currentCar = newCar;
+	}
+	
+	/**
+	 * Allows a player to change username along with make, model and color of their car.
+	 * Prints out a menu giving the player different options.
+	 */
+	public void updateProfile() {
+		boolean done = false;
+		String menuItem;
+		String newName;
+		String carMake;
+		String carModel;
+		String carColor;
+		Color newColor;
+		
+		try (Scanner userInput = new Scanner(System.in)) {
+			
+			while (!done) {
+				System.out.println("===============================================");
+				System.out.println("\t\tUPDATE PROFILE MENU");
+				System.out.println("===============================================");
+				System.out.println("1. Change Username");
+				System.out.println("2. Change Car Make");
+				System.out.println("3. Change Car Model");
+				System.out.println("4. Change Car Color");
+				System.out.println("0. Exit");
+				
+				System.out.print("\nPlease enter a menu option: ");
+				menuItem = userInput.next();
+				
+				switch(menuItem) {
+				case "1" :
+					System.out.println("Type in your new username: ");
+		            newName = userInput.next();
+		            this.setUserName(newName);
+		            break;
+		        case "2" :
+		            System.out.print("\nWhat is the make of your car: ");
+		            carMake = userInput.next();
+		            this.currentCar.setMake(carMake);
+		            break;
+		        case "3" :
+		            System.out.print("\nWhat is the model of your car: ");
+		            carModel = userInput.next();
+		            this.currentCar.setModel(carModel);
+		            break;
+		        case "4" :
+		            System.out.print("\nWhat is the color of your car: ");
+		            carColor = userInput.next();
+		            
+		            try {
+		                newColor = (Color) Color.class.getField(carColor.toLowerCase()).get(null);
+		            } catch (Exception exception) {
+		            	newColor = null; // Not defined
+		            	System.out.println("We're sorry, " + carColor + " is not a valid color.");
+		            }
+		            this.currentCar.setColor(newColor);
+		            break;
+		        case "0" :
+		            System.out.println("Exiting profile update.\n");
+		            done = true;
+		            break;
+		        default :
+		            System.out.println("Please try again with a valid number.");
+		            break;
+				}
+			}
+		}
 	}
 }
