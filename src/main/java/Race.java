587d9898a419043692b74699f97f550cae9d95d4
Joel Christiansen <greenbadge.jc@gmail.com>
2016-04-27 16:15:15 -0700

    removed Stats, added javadocs to Race

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -20,6 +20,9 @@ public class Race {
 	private boolean raceWon;
 	private int raceSlotsLeft;
 	
+	/**
+	 * Race Initializer 
+	 */
 	public Race() {
 		
 		players = new ArrayList<Player>();
@@ -29,13 +32,29 @@ public class Race {
 		raceSlotsLeft = 12;
 	}
 	
+	/**
+	 * Creates the race. Each player in a race needs to call this.
+	 * @param player The player joining the race
+	 * @param track The track being played
+	 */
 	public void makeRace(Player player, Track track){
 		joinRace(player); 
 		this.track = track;
 	}
+	
+	/**
+	 * 
+	 * @return The number of people that can still join the race.
+	 */
 	public int getRaceSlotsLeft(){
 		return raceSlotsLeft;
 	}
+	
+	/**
+	 * 
+	 * @param player The player joining the race
+	 * @return True if the player joined the race. False otherwise
+	 */
 	public boolean joinRace(Player player){
 		boolean joinSuccess = false;
 		if (raceSlotsLeft > 0){
@@ -47,6 +66,10 @@ public class Race {
 		return joinSuccess;
 	}
 	
+	/**
+	 * Adds a player to the race
+	 * @param player The player being added to the race.
+	 */
 	private void addPlayerToRace(Player player){
 		String userName = player.getName();
 		int[] raceStatuses = setPlayerStatus(0,0);
@@ -55,6 +78,12 @@ public class Race {
 		playerRaceStatus.put(userName, raceStatuses);
 	}
 	
+	/**
+	 * Sets position and if they player will be skipped for the next n rounds
+	 * @param position The current position of the player
+	 * @param skipStatus The number of rounds the player will be skipped
+	 * @return [position, number of turns to be skipped]
+	 */
 	public int[] setPlayerStatus(int position, int skipStatus){
 		int[] raceStatuses = new int[2];
 		int racePosition = position;
@@ -67,11 +96,19 @@ public class Race {
 	}
 	
 	
+	/**
+	 * Starts the race
+	 */
 	public void startRace(){
 		raceStarted =  true;
 	}
 	
 	
+	/**
+	 * Moves the car
+	 * @param player The player who's car is being moved
+	 * @param dieTotal the number of places to move the car
+	 */
 	public void moveCar(Player player, int dieTotal){
 		int usersNewPosition = determineUsersNewPositon(player, dieTotal);
 		raceWon = (usersNewPosition > track.getDistance())?  true : false; 
@@ -79,6 +116,13 @@ public class Race {
 		//track.checkForRoadObstruction(usersNewPosition); 
 		//track.userNumberOfTurnsUserSkipped()
 	}
+	
+	/**
+	 * 
+	 * @param player The player who's position is being changed
+	 * @param dieTotal The result for the dice roll
+	 * @return The position of the player after the dice roll
+	 */
 	private int determineUsersNewPositon(Player player, int dieTotal){
 		int[] playerStatuses = playerRaceStatus.get(player.getName());
 		Car playersCar = player.getCurrentCar();
