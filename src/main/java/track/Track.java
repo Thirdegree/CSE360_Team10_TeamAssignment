743543e985edd9150d841e8de37e475695d578df
Joel Christiansen <greenbadge.jc@gmail.com>
2016-04-29 15:17:32 -0700

    added overloaded constructor for track

--- src/main/java/track/Track.java
+++ src/main/java/track/Track.java
@@ -10,18 +10,39 @@ public class Track {
 	private String endLocation;
 	private int distance;
 	
+	/**
+	 * First initializer. 
+	 */
 	public Track(){
 		startLocation = "Phoenix, AZ";
 		endLocation = "Austin, TX";
 		distance = 1006;
 	} 
 	
+	/**
+	 * Second initializer. 
+	 */
+	public Track(String startLocation, String endLocation, int distance) {
+		this.startLocation = startLocation;
+		this.endLocation = endLocation;
+		this.distance = distance;
+	}
+	
+	/*
+	 * Returns the start location
+	 */
 	public String getStartLocation() {
 		return startLocation;
 	}
+	/*
+	 * Returns the end location
+	 */
 	public String getEndLocation() {
 		return endLocation;
 	}
+	/*
+	 * Returns the distance.
+	 */
 	public int getDistance() {
 		return distance;
 	}
